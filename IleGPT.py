b777853753a530fb405c5be7faf05c48f24e7a12
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-08-04 17:37:36 +0300

    Chapter 4 (GELU activation class)

--- IleGPT.py
+++ IleGPT.py
@@ -68,6 +68,24 @@ class LayerNorm(nn.Module):
         return self.scale * norm_x + self.shift
 
 
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return (
+            0.5
+            * x
+            * (
+                1
+                + torch.tanh(
+                    torch.sqrt(torch.tensor(2.0 / torch.pi))
+                    * (x + 0.44715 * torch.pow(x, 3))
+                )
+            )
+        )
+
+
 # tokenize a batch consisting of two inputs
 import tiktoken
 
