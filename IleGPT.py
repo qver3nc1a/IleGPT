5399a67c12f3f1cd05414eafa1cbb6f5f09dbce3
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-08-04 17:12:33 +0300

    Chapter 4 (layer normalization class)

--- IleGPT.py
+++ IleGPT.py
@@ -52,6 +52,22 @@ class DummyLayerNorm(nn.Module):
         return x
 
 
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        # small constant added to var to prevent division by 0
+        self.eps = 1e-5
+        # trainable parameters automatically adjusted during training
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
 # tokenize a batch consisting of two inputs
 import tiktoken
 
@@ -89,5 +105,13 @@ out_norm = (out - mean) / torch.sqrt(var)
 mean = out_norm.mean(dim=-1, keepdim=True)
 var = out_norm.var(dim=-1, keepdim=True)
 print("Normalized layer outputs:\n", out_norm)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# normalization with LayerNorm module
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 print("Mean:\n", mean)
 print("Variance:\n", var)
