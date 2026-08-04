d3b6a9e58b11bc391554e182f4aa96c1e08d65b3
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-08-04 17:47:31 +0300

    Chapter 4 (GELU vs ReLU plot)

--- IleGPT.py
+++ IleGPT.py
@@ -133,3 +133,22 @@ mean = out_ln.mean(dim=-1, keepdim=True)
 var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 print("Mean:\n", mean)
 print("Variance:\n", var)
+
+# plot ReLU vs GELU
+import matplotlib.pyplot as plt
+
+gelu, relu = GELU(), nn.ReLU()
+
+x = torch.linspace(-3, 3, 100)  # 100 sample data points (range -3 to 3)
+y_gelu, y_relu = gelu(x), relu(x)
+plt.figure(figsize=(8, 3))
+
+for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+    plt.subplot(1, 2, i)
+    plt.plot(x, y)
+    plt.title(f"{label} activation function")
+    plt.xlabel("x")
+    plt.ylabel(f"{label}(x)")
+    plt.grid(True)
+plt.tight_layout()
+plt.show()
