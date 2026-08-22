9c865d4c7b3d024b7a4ddfbb8dae58efa62b0ed7
Iliana Dulina <ilyana.dulina07@gmail.com>
2026-08-22 19:45:12 +0300

    underscore underscore

--- Chapter4.py
+++ Chapter4.py
@@ -208,23 +208,22 @@ sample_input = torch.tensor([1.0, 0.0, -1.0])
 torch.manual_seed(123)
 model_without_shortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=False)
 
+if __name__ == "__main__":
+    # function that computes gradients in backward pass
+    def print_gradients(model, x):
+        # forward pass
+        output = model(x)
+        target = torch.tensor([[0.0]])
 
-# function that computes gradients in backward pass
-def print_gradients(model, x):
-    # forward pass
-    output = model(x)
-    target = torch.tensor([[0.0]])
+        # loss
+        loss = nn.MSELoss()
+        loss = loss(output, target)
 
-    # loss
-    loss = nn.MSELoss()
-    loss = loss(output, target)
+        # backward pass (calculate gradients)
+        loss.backward()
 
-    # backward pass (calculate gradients)
-    loss.backward()
+        for name, param in model.named_parameters():
+            if "weight" in name:
+                print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
 
-    for name, param in model.named_parameters():
-        if "weight" in name:
-            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
-
-
-print_gradients(model_without_shortcut, sample_input)
+    print_gradients(model_without_shortcut, sample_input)
